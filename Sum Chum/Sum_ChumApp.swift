7bd52f020e9bc1b3db3374d6c77ac8833c7d6308
callum day ham <caldayham@gmail.com>
2021-06-05 13:41:30 -0700

    Initial Commit

---
+++ Sum Chum/Sum_ChumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sum_ChumApp.swift
+//  Sum Chum
+//
+//  Created by Callum Day Ham on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Sum_ChumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
